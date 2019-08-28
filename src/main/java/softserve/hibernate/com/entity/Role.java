aaf4c68330cd6fbdf63004b94f73d021059ad1fe
Andrii Torzhkov <andrejtorzhkov94@gmail.com>
2019-08-28 14:03:19 +0300

    develop: added spring-data-jpa

--- src/main/java/softserve/hibernate/com/entity/Role.java
+++ src/main/java/softserve/hibernate/com/entity/Role.java
@@ -2,18 +2,19 @@ package softserve.hibernate.com.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "roles")
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String roleType;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", columnDefinition="ENUM('GUEST','USER','ADMIN')", nullable = false)
+    private RoleType roleType;
+
 }
--- src/main/java/softserve/hibernate/com/entity/User.java
+++ src/main/java/softserve/hibernate/com/entity/User.java
@@ -2,15 +2,11 @@ package softserve.hibernate.com.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
