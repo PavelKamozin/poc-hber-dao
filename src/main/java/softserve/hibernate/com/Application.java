1bc0316edc542bbfd825d16a97dd23636ae16fde
Andrii Torzhkov <andrejtorzhkov94@gmail.com>
2019-08-29 12:47:26 +0300

    Merge branch 'develop' of /home/atorz/IdeaProjects/poc-hber-dao with conflicts.

--- src/main/java/softserve/hibernate/com/Application.java
+++ src/main/java/softserve/hibernate/com/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import softserve.hibernate.com.dao.impl.RoleDaoImpl;
-import softserve.hibernate.com.dao.impl.UserDaoImpl;
+import softserve.hibernate.com.entity.Role;
 import softserve.hibernate.com.repository.RoleRepository;
 
 import java.util.logging.Logger;
@@ -21,9 +21,6 @@ public class Application implements CommandLineRunner {
 
     private static final Logger log = Logger.getLogger(Application.class.getName());
 
-    @Autowired
-    private UserDaoImpl userDao;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
@@ -31,6 +28,27 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) {
         log.info("Hello world");
+
+        roleRepository.deleteAll();
+
+        Role role = new Role();
+        role.setRoleType(RoleType.ADMIN);
+
+        roleDao.create(role);
+
+        Role role1 = new Role();
+        role1.setRoleType(RoleType.USER);
+
+        roleDao.create(role1);
+
+        Role role2 = new Role();
+        role2.setRoleType(RoleType.GUEST);
+
+        roleDao.create(role2);
+
+        System.out.println(roleDao.searchByQuery("role is not null", PageRequest.of(0 ,2)));
+
+        roleRepository.deleteAll();
     }
 
 }
